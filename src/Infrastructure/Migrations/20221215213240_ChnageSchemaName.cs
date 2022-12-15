139a581890fa24631964a4caf531e72731d8fdfd
Dawid Wijata <dawid.wijata@hotmail.com>
2022-12-15 22:44:13 +0100

    Add migration files

---
+++ src/Infrastructure/Migrations/20221215213240_ChnageSchemaName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class ChnageSchemaName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "user");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "Users",
+                newSchema: "user");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Users",
+                schema: "user",
+                newName: "Users");
+        }
+    }
+}
